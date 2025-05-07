8c202853c856b54388c4b5d7ebd08a29326451f7
Shreeniwas <shreeniwaschate121@gmail.com>
2025-05-07 21:41:57 +0530

    Trigger poll SCM test

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,5 +2,7 @@ public class HelloStudent {
     public static void main(String[] args) {
         System.out.println("Hello, this is us Shree and Vikram!");
         System.out.println("Welcome to C-DAC");
+         System.out.println("SCM test");
+
     }
 }
